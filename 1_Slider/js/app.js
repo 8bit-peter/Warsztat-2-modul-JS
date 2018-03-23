15df7024b6fb34ddaf1dc24c7043839f425c3cb2
Piotr Jaczewski <pp.jaczewski@gmail.com>
2018-03-23 14:30:07 +0100

    Slider wykonany

--- 1_Slider/js/app.js
+++ 1_Slider/js/app.js
@@ -1,3 +1,36 @@
 /**
  * Created by Jacek on 2015-12-16.
  */
+
+document.addEventListener("DOMContentLoaded", function(){
+
+var prev = document.querySelector('#prevPicture');
+var next = document.querySelector('#nextPicture');
+var listaLi = document.querySelectorAll('li');
+var counter = 0
+
+listaLi[0].className = 'visible';
+
+next.addEventListener('click', function(event) {
+    listaLi[counter].classList.remove('visible');
+    counter++
+    
+    if(counter === listaLi.length-1) {
+        counter = 0
+    }
+
+    listaLi[counter].classList.add('visible');
+})
+
+prev.addEventListener('click', function(event) {
+    listaLi[counter].classList.remove('visible');
+    counter--
+
+    if(counter < 0) {
+        counter = listaLi.length-1
+    }
+    
+    listaLi[counter].classList.add('visible');
+})
+
+});
