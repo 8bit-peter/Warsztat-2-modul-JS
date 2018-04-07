a6c3603ed15e8ee4456cc7892076486cc38d2605
Piotr Jaczewski <pp.jaczewski@gmail.com>
2018-04-07 17:40:21 +0200

    Zadanie 5 tooltip

--- 5_Tooltips/js/app.js
+++ 5_Tooltips/js/app.js
@@ -6,3 +6,33 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+
+document.addEventListener("DOMContentLoaded", function(){
+    
+    var toolTip = document.querySelectorAll('.tooltip');
+
+
+    for( var i = 0; i < toolTip.length; i++) {
+
+        toolTip[i].addEventListener('mouseover', function(event) {
+            var text = this.dataset.text;
+            var tpSpan = document.createElement('span');
+            tpSpan.classList.add('tooltipText');
+            tpSpan.innerText = text;
+
+            this.appendChild(tpSpan);
+        })
+
+    }
+
+    for( var i = 0; i < toolTip.length; i++) {
+
+        toolTip[i].addEventListener('mouseout', function(event) {
+            var tpSpanOut = this.firstElementChild;
+
+            tpSpanOut.parentElement.removeChild(tpSpanOut);
+        })
+
+    }
+
+});
