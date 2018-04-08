39d9ab292ea9406b42851ffe4c40ce5170d55739
Piotr Jaczewski <pp.jaczewski@gmail.com>
2018-04-08 15:41:32 +0200

    Warsztat JS - ToDo bez walidacji

--- 6_ToDoList/js/app.js
+++ 6_ToDoList/js/app.js
@@ -3,7 +3,58 @@
  */
 
 
+document.addEventListener("DOMContentLoaded", function(){
 
+    var addTaskButton = document.querySelector('#addTaskButton');
+    var taskList = document.querySelector('#taskList');
+    var counter = 0;
+    var removeButton = document.querySelector('#removeFinishedTasksButton');
+
+    
+    addTaskButton.addEventListener('click', function (event) {
+
+        var taskInput = document.querySelector('#taskInput');
+
+        var newTask = document.createElement('li');
+        var taskHeader = document.createElement('h1');
+        var taskText = document.createElement('p');
+        var btnDelete = document.createElement('button');
+        var btnComplete = document.createElement('button');
+
+        newTask.innerText = 'Task ' + ++counter;
+        taskText.innerText = taskInput.value;
+        btnDelete.innerText = 'X';
+        btnComplete.innerText = 'OK';
+
+        btnDelete.classList.add('btn');
+        btnComplete.classList.add('btn');
+
+        newTask.appendChild(taskHeader);
+        newTask.appendChild(taskText);
+        newTask.appendChild(btnDelete);
+        newTask.appendChild(btnComplete);
+        taskList.appendChild(newTask);
+
+        taskInput.value = '';
+
+        btnComplete.addEventListener('click', function (event) {
+            newTask.classList.toggle('done');
+        });
+
+        btnDelete.addEventListener('click', function (event) {
+            newTask.parentElement.removeChild(newTask);
+            counter--;
+        });
+    });
+
+    removeButton.addEventListener('click', function (event) {
+       var done = document.querySelectorAll('.done');
+       for (var i = 0; i < done.length; i++) {
+           done[i].parentElement.removeChild(done[i]);
+           counter--;
+       }
+    });
+});
 
 
 
